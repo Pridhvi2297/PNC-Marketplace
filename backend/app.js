bbdc64867bf15f61a7a2b6fa6c29c2ef12a3f6e6
prudhvi2297 <mpnc2296@gmail.com>
2023-11-11 06:14:55 -0500

    Backend setup for user model and multer

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({extended:true, limit:"50mb"}));
+
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
@@ -8,4 +17,12 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 
+const user = require("./controller/user");
+
+
+
+app.use("/api/v2/user", user);
+
+
+app.use(ErrorHandler);
 module.exports = app;
---
+++ backend/controller/user.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const { upload } = require("../multer");
+const User = require("../model/user");
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const router = express.Router();
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const sendMail = require("../utils/sendMail");
+const user = require("../model/user");
+const sendToken = require("../utils/jwtToken");
+const { isAuthenticated } = require("../middleware/auth");
+
+router.post("/create-user", upload.single("file"), async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const userEmail = await User.findOne({ email });
+
+    if (userEmail) {
+      const filename = req.file.filename;
+      const filePath = `uploads/${filename}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: "Error deleting file" });
+        }
+      });
+      return next(new ErrorHandler("User already exists", 400));
+    }
+
+    const filename = req.file.filename;
+    const fileUrl = path.join(filename);
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      avatar: fileUrl,
+    };
+
+    const activationToken = createActivationToken(user);
+
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+
+    try {
+      await sendMail({
+        email: user.email,
+        subject: "Activate your account",
+        message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+      });
+      res.status(201).json({
+        success: true,
+        message: `Please check your email ${user.email} to activate your account!`,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+});
+
+module.exports = router;
