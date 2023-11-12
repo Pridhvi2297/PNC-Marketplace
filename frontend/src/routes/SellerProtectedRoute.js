2ede35eafeb5c880b78a1ea26b4c9dd6b6fb7f24
prudhvi2297 <mpnc2296@gmail.com>
2023-11-12 07:45:00 -0500

    Fix-Frontend SellerProtectedRoute

--- frontend/src/routes/SellerProtectedRoute.js
+++ frontend/src/routes/SellerProtectedRoute.js
@@ -4,12 +4,12 @@ import { Navigate } from "react-router-dom";
 const SellerProtectedRoute = ({ children }) => {
   const { isLoading, isSeller } = useSelector((state) => state.seller);
 
-
+  if (isLoading === false) {
     if (!isSeller) {
       return <Navigate to={`/shop-login`} replace />;
     }
     return children;
-
+  }
 };
 
 export default SellerProtectedRoute;
