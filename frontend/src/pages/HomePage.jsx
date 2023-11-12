20f9affec7b9028b1ab75c9226d4cc071aa48909
prudhvi2297 <mpnc2296@gmail.com>
2023-11-12 03:49:32 -0500

    Frontend HomePage Hero

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Header from "../components/Layout/Header.jsx";
+import Header from "../components/Layout/Header";
+import Hero from "../components/Route/Hero/Hero";
 
 
 const HomePage = () => {
   return (
     <div>
         <Header activeHeading={1} />
-
+        <Hero />
     </div>
   )
 }
