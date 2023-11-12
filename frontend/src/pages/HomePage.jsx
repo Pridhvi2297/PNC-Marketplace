3ebf4825534b28a50abf219758af846c752618af
prudhvi2297 <mpnc2296@gmail.com>
2023-11-12 03:53:12 -0500

    Frontend HomePage Deals ProductCard ProductDetailsCard

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from "../components/Layout/Header";
 import Hero from "../components/Route/Hero/Hero";
-
+import Deals from "../components/Route/Deals/Deals";
 
 const HomePage = () => {
   return (
     <div>
         <Header activeHeading={1} />
         <Hero />
+        <Deals />
+
     </div>
   )
 }
